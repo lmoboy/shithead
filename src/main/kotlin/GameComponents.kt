b8fd0a7e483df0a2c0b7a7cd1b23381090410c70
lmoboy <steamgoyu19@gmail.com>
2024-12-31 00:45:36 +0200

    Basically made no progress but i am getting there!!!

--- src/main/kotlin/GameComponents.kt
+++ src/main/kotlin/GameComponents.kt
@@ -1,6 +1,7 @@
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
@@ -13,7 +14,9 @@ fun Hand(
         horizontalArrangement = Arrangement.SpaceBetween,
         modifier = Modifier.fillMaxWidth(),
     ) {
-
+        for (card in cards) {
+            Text(card)
+        }
     }
 }
 
